1517521573863a0213483741b459af701eabb580
Andrei Sergeev <Andrei.Sergeev@infobip.com>
2026-01-18 16:34:35 +0300

    solve 94

---
+++ src/leetcode/easy/a_94.rs
@@ -0,0 +1,66 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    if root == None { return vec![] }
+    let rc_node = root.clone().unwrap();
+    let node = rc_node.borrow();
+    [inorder_traversal(node.left.clone()), vec![node.val], inorder_traversal(node.right.clone())].concat()
+}
+
+#[test]
+fn test_bfs() {
+    let one = Some(Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: None,
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+            right: None,
+        }))),
+    })));
+    assert_eq!(inorder_traversal(one), vec![1, 3, 2]);
+    let two = Some(Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode::new(6)))),
+                right: Some(Rc::new(RefCell::new(TreeNode::new(7)))),
+            }))),
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 8,
+                left: Some(Rc::new(RefCell::new(TreeNode::new(9)))),
+                right: None,
+            }))),
+        }))),
+    })));
+    assert_eq!(inorder_traversal(two), vec![4, 2, 6, 5, 7, 1, 3, 9, 8]);
+    assert_eq!(inorder_traversal(Some(Rc::new(RefCell::new(TreeNode::new(1))))), vec![1]);
+    assert_eq!(inorder_traversal(None), vec![]);
+}
